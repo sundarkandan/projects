45dc654b5ced378aa5634c2d3ddaec69ec65a093
sundarkandan <sundarkandan23506@gmail.com>
2025-10-06 20:34:54 +0530

    Delete theatre directory

--- theatre/client/src/text.js
+++
@@ -1,5 +0,0 @@
-
-  const phone = '919597732047'; // your number
-  const msg = encodeURIComponent('Hi, I’m interested in your product!');
-  window.open(`https://wa.me/${phone}?text=${msg}`, '_blank');
-
